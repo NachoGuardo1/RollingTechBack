0352800890a77d942bd362e2e7e72eb5471544fd
NachoGuardo1 <118404932+NachoGuardo1@users.noreply.github.com>
2023-07-17 20:04:27 -0300

    Revert "Anitaiturre"

--- models/rol.js
+++
@@ -1,8 +0,0 @@
-const {Schema,model}=require('mongoose');
-const RolSchema = Schema({
-    rol:{
-        type: String, requerid: [true,"El rol es obligatorio"]
-    }
-})
-
-module.exports=model("rol",RolSchema);
