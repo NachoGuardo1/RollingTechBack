275f6c3a54976e71586d2576a8d1d7c378be370d
Ignacio Guardo <nachoguardoruiz@gmail.com>
2023-08-05 16:25:17 -0300

    cambio config server

--- models/server.js
+++ models/server.js
@@ -1,58 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../database/config");
 
-const express = require('express')
-const {dbConnection} = require('../database/config')
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.authPath = "/api/auth";
+    this.usuariosPath = "/api/usuarios";
+    this.productosPath = "/api/productos";
+    this.categoriasPath = "/api/categoria";
+    this.buscarPath = "/api/buscar";
 
-class Server{
-    constructor(){
-        this.app = express();
-        this.port=process.env.PORT;
-        this.authPath="/api/auth";
-        this.usuariosPath ='/api/usuarios';
-        this.productosPath ='/api/productos';
-        this.categoriasPath='/api/categoria';
-        this.buscarPath='/api/buscar';
+    //conecatar con BD
+    this.conectarDB();
+    //middlewares
+    this.middlewares();
 
-        //conecatar con BD
-        this.conectarDB()
-        //middlewares
-        this.middlewares();
+    //funcion para las rutasc
+    this.routes();
+  }
 
-        //funcion para las rutas
-        this.routes();
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    }
+  middlewares() {
+    this.app.use((req, res, next) => {
+      res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://rolling-tech.vercel.app"
+      );
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      next();
+    });
+    //leer lo q el usuario envia por el cuerpo de la peticion
+    this.app.use(express.json());
+    //definir la carpeta publica
+    this.app.use(express.static("public"));
+  }
 
-    async conectarDB(){
-        await dbConnection()
-    }
+  routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+    this.app.use(this.categoriasPath, require("../routes/categorias"));
+    this.app.use(this.productosPath, require("../routes/productos"));
+    this.app.use(this.buscarPath, require("../routes/buscar"));
+  }
 
-    middlewares(){
-      
-        //leer lo q el usuario envia por el cuerpo de la peticion
-        this.app.use(express.json());
-        //definir la carpeta publica
-        this.app.use(express.static('public'));
-
-    }
-
-    routes(){
-        this.app.use(this.authPath,require('../routes/auth'));
-        this.app.use(this.usuariosPath,require('../routes/usuarios'));
-        this.app.use(this.categoriasPath,require('../routes/categorias'));
-        this.app.use(this.productosPath,require('../routes/productos'));
-        this.app.use(this.buscarPath,require('../routes/buscar'));
-
-    }
-    
-    listen(){
-
-    
-        this.app.listen(this.port,() =>{
-            console.log('Server online por puerto', this.port)
-
-        })
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server online por puerto", this.port);
+    });
+  }
 }
 
 module.exports = Server;
