19da355c14c64fc824b3579f20d5f50fa2a8b6db
Ignacio Guardo <nachoguardoruiz@gmail.com>
2023-08-05 16:34:05 -0300

    config server

--- models/server.js
+++ models/server.js
@@ -27,10 +27,7 @@ class Server {
 
   middlewares() {
     this.app.use((req, res, next) => {
-      res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://rolling-tech.vercel.app"
-      );
+      res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.setHeader(
         "Access-Control-Allow-Headers",
