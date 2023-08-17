ee88833f7a4f37b73a29877ad1f737d1497be6e5
aeiturre <aeiturre@gmail.com>
2023-08-17 16:53:24 -0300

    default get on api setted

--- models/server.js
+++ models/server.js
@@ -38,6 +38,7 @@ class Server{
     }
 
     routes(){
+        this.app.get('/', (req, res) => res.send('server up!'));
         this.app.use(this.authPath,require('../routes/auth'));
         this.app.use(this.usuariosPath,require('../routes/usuarios'));
         this.app.use(this.categoriasPath,require('../routes/categorias'));
